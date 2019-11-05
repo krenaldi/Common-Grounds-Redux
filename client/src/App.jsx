79ace41545edade2962feb22ed540c33b3ee000d
Kris Renaldi <krenaldi@comcast.net>
2019-11-04 21:42:20 -0500

    Updated App component to include Test page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import MenuBar from './components/menus/MenuBar';
 import Group from './pages/Group';
+import Test from './pages/Test';
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
                 <Route path="/" component={MenuBar} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/group" component={Group} />
+                <Route path="/admin" component={Test} />
             </UserProvider>
             <Route exact path="/" component={Home} />
         </Router>
