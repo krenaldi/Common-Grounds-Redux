76aa04e90ef57e01cbe42aa5d92f5914741fbdf4
Kris Renaldi <krenaldi@comcast.net>
2019-11-04 22:38:24 -0500

    Added Test & TestResults pages for testing social media logins via admin route

--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -5,17 +5,13 @@ import CardList from '../components/cards/CardList';
 const Home = () => {
     return (
         <div className="page" style={{ textAlign: "center"}}>
-            <p className="page-title">Simple OAuth with Node</p>
             <p style={{ fontSize: 20 }}>
-                Login with Facebook, Google, or Github.
+                Test Login with Facebook, Google, or Github.
             </p>
             <Terminal 
                 userData={"passport.authenticate('facebook')"} 
                 selected="All"
             />
-            <p style={{ fontSize: 28 }}>
-                Popular Login Strategies
-            </p>
             <CardList />
             <div style={{ marginBottom: 20 }} />
         </div>
