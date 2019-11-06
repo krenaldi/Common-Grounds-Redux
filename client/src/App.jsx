2157dee578dcdfdde1d1f57fd5c863e0f57fc777
Kris Renaldi <krenaldi@comcast.net>
2019-11-05 23:06:27 -0500

    Updated Profile page to display name from DB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/Home';
 import Profile from './pages/Profile';
 import MenuBar from './components/menus/MenuBar';
 import Group from './pages/Group';
-import Data from './pages/DataResults';
+// import Data from './pages/DataResults';
 
 const App = () => {
     return (
@@ -15,7 +15,7 @@ const App = () => {
                 <Route path="/" component={MenuBar} />
                 <Route path="/profile" component={Profile} />
                 <Route path="/group" component={Group} />
-                <Route path="/profile" component={Data} />
+                {/* <Route path="/profile" component={Data} /> */}
             </UserProvider>
             <Route exact path="/" component={Home} />
         </Router>
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
 export default {
-    // Gets all books
+    // Create group
     CreateGroup: function (data) {
         return axios.post("/api/group", data);
+    },
+    // Get Profile data
+    getProfile: function(id) {
+        return axios.get("/user");
+    },
+    // Update Profile data
+    updateProfile: function(userData) {
+        return axios.post("/user", userData);
     }
 }
  
